6058f47103c284fa5fa567db3b8a76371f0bace3
AshimaKamra <57401395+AshimaKamra@users.noreply.github.com>
2019-11-22 13:21:52 +0530

    Update cocktail.js

--- cocktail.js
+++ cocktail.js
@@ -37,7 +37,6 @@ function load1()
 {
 
     var xmlhttp,x, txt = "";
-    var des="";
     var obj = { "table":"drinks", "limit":10 };
     var dbParam = JSON.stringify(obj);
     xmlhttp = new XMLHttpRequest();
@@ -47,16 +46,16 @@ function load1()
 {
   var myObj=JSON.parse(this.responseText);
        txt +="<div class=\"cocktails heading2\">"
-       des +="<div class=\"ingredients\">"
-        for(x in myObj.ingredients)
+     
+        for(x in myObj.drinks)
            {
                
-             txt += myObj.ingredients[x].strIngredient + "<br>";
-            // des += myObj.ingredients[x].strDescription + "<br>";
+             txt += myObj.drinks[x].strIngredient1 + "<br>";
+          
             }
             document.getElementById("demo3").innerHTML = txt;
-            document.getElementById("demo4").innerHTML = des;
-           des +="</div>"
+          
+      
             txt +="</div>";
 
         
